a04b047f08682a129342e4110dbf666f076fdf8e
Prajyot5135 <130391558+prajyotpawar5135@users.noreply.github.com>
2023-07-12 17:32:39 +0530

    ~Basic .net program using c#

---
+++ DrawingPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using  Mathematics;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Point startPoint=new Point(15,12);
+Point endPoint=new Point(150,120);
+
+Shape l1=new Line(startPoint, endPoint, 5, "red");
+
+
+l1.draw();
+
+
+
+
+
+
+
+
+
