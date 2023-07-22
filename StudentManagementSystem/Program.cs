7da678ac8dc0e63cf79e9445ae42643aeea861c5
Prajyot5135 <130391558+prajyotpawar5135@users.noreply.github.com>
2023-07-22 18:20:51 +0530

    Add files via upload

---
+++ StudentManagementSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BOLl;
+using DALi;
+
+Console.WriteLine("Hello, prajyot!");
+
+
+List<Student> students=DALi.Class1.getAllStudents();
+foreach (var st in students)
+{
+    Console.WriteLine(st);
+}
